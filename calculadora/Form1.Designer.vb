e38b7e23c28a2594681b3eda7ff106287604b823
geovani_santos <geovanny12016@gmail.com>
2019-04-15 23:34:07 -0600

    calculadoraValidaciones

--- calculadora/Form1.Designer.vb
+++ calculadora/Form1.Designer.vb
@@ -44,9 +44,9 @@ Partial Class Form1
         '
         'ButtonSIETE
         '
-        Me.ButtonSIETE.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ButtonSIETE.BackColor = System.Drawing.Color.Black
         Me.ButtonSIETE.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ButtonSIETE.ForeColor = System.Drawing.Color.Red
+        Me.ButtonSIETE.ForeColor = System.Drawing.Color.White
         Me.ButtonSIETE.Location = New System.Drawing.Point(12, 90)
         Me.ButtonSIETE.Name = "ButtonSIETE"
         Me.ButtonSIETE.Size = New System.Drawing.Size(56, 49)
@@ -56,176 +56,211 @@ Partial Class Form1
         '
         'ButtonCINCO
         '
+        Me.ButtonCINCO.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonCINCO.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCINCO.ForeColor = System.Drawing.Color.White
         Me.ButtonCINCO.Location = New System.Drawing.Point(87, 157)
         Me.ButtonCINCO.Name = "ButtonCINCO"
         Me.ButtonCINCO.Size = New System.Drawing.Size(56, 49)
         Me.ButtonCINCO.TabIndex = 1
         Me.ButtonCINCO.Text = "5"
-        Me.ButtonCINCO.UseVisualStyleBackColor = True
+        Me.ButtonCINCO.UseVisualStyleBackColor = False
         '
         'ButtonTRES
         '
+        Me.ButtonTRES.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonTRES.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonTRES.ForeColor = System.Drawing.Color.White
         Me.ButtonTRES.Location = New System.Drawing.Point(159, 221)
         Me.ButtonTRES.Name = "ButtonTRES"
         Me.ButtonTRES.Size = New System.Drawing.Size(56, 49)
         Me.ButtonTRES.TabIndex = 2
         Me.ButtonTRES.Text = "3"
-        Me.ButtonTRES.UseVisualStyleBackColor = True
+        Me.ButtonTRES.UseVisualStyleBackColor = False
         '
         'ButtonDOS
         '
+        Me.ButtonDOS.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonDOS.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDOS.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.ButtonDOS.Location = New System.Drawing.Point(87, 221)
         Me.ButtonDOS.Name = "ButtonDOS"
         Me.ButtonDOS.Size = New System.Drawing.Size(56, 49)
         Me.ButtonDOS.TabIndex = 3
         Me.ButtonDOS.Text = "2"
-        Me.ButtonDOS.UseVisualStyleBackColor = True
+        Me.ButtonDOS.UseVisualStyleBackColor = False
         '
         'ButtonResultado
         '
-        Me.ButtonResultado.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonResultado.BackColor = System.Drawing.Color.White
+        Me.ButtonResultado.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonResultado.ForeColor = System.Drawing.Color.Red
         Me.ButtonResultado.Location = New System.Drawing.Point(174, 285)
         Me.ButtonResultado.Name = "ButtonResultado"
         Me.ButtonResultado.Size = New System.Drawing.Size(56, 49)
         Me.ButtonResultado.TabIndex = 4
         Me.ButtonResultado.Text = "="
-        Me.ButtonResultado.UseVisualStyleBackColor = True
+        Me.ButtonResultado.UseVisualStyleBackColor = False
         '
         'ButtonSuma
         '
-        Me.ButtonSuma.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSuma.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ButtonSuma.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSuma.ForeColor = System.Drawing.Color.White
         Me.ButtonSuma.Location = New System.Drawing.Point(236, 285)
         Me.ButtonSuma.Name = "ButtonSuma"
         Me.ButtonSuma.Size = New System.Drawing.Size(56, 49)
         Me.ButtonSuma.TabIndex = 5
         Me.ButtonSuma.Text = "+"
-        Me.ButtonSuma.UseVisualStyleBackColor = True
+        Me.ButtonSuma.UseVisualStyleBackColor = False
         '
         'ButtonPunto
         '
+        Me.ButtonPunto.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonPunto.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPunto.ForeColor = System.Drawing.Color.White
         Me.ButtonPunto.Location = New System.Drawing.Point(74, 285)
         Me.ButtonPunto.Name = "ButtonPunto"
         Me.ButtonPunto.Size = New System.Drawing.Size(45, 49)
         Me.ButtonPunto.TabIndex = 6
         Me.ButtonPunto.Text = "."
-        Me.ButtonPunto.UseVisualStyleBackColor = True
+        Me.ButtonPunto.UseVisualStyleBackColor = False
         '
         'Button0
         '
+        Me.Button0.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Button0.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.ForeColor = System.Drawing.Color.White
         Me.Button0.Location = New System.Drawing.Point(12, 285)
         Me.Button0.Name = "Button0"
         Me.Button0.Size = New System.Drawing.Size(56, 49)
         Me.Button0.TabIndex = 7
         Me.Button0.Text = "0"
-        Me.Button0.UseVisualStyleBackColor = True
+        Me.Button0.UseVisualStyleBackColor = False
         '
         'ButtonUNO
         '
+        Me.ButtonUNO.BackColor = System.Drawing.Color.Black
         Me.ButtonUNO.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonUNO.ForeColor = System.Drawing.Color.White
         Me.ButtonUNO.Location = New System.Drawing.Point(12, 221)
         Me.ButtonUNO.Name = "ButtonUNO"
         Me.ButtonUNO.Size = New System.Drawing.Size(56, 49)
         Me.ButtonUNO.TabIndex = 8
         Me.ButtonUNO.Text = "1"
-        Me.ButtonUNO.UseVisualStyleBackColor = True
+        Me.ButtonUNO.UseVisualStyleBackColor = False
         '
         'ButtonCUATRO
         '
+        Me.ButtonCUATRO.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonCUATRO.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCUATRO.ForeColor = System.Drawing.Color.White
         Me.ButtonCUATRO.Location = New System.Drawing.Point(12, 157)
         Me.ButtonCUATRO.Name = "ButtonCUATRO"
         Me.ButtonCUATRO.Size = New System.Drawing.Size(56, 49)
         Me.ButtonCUATRO.TabIndex = 9
         Me.ButtonCUATRO.Text = "4"
-        Me.ButtonCUATRO.UseVisualStyleBackColor = True
+        Me.ButtonCUATRO.UseVisualStyleBackColor = False
         '
         'ButtonSEIS
         '
+        Me.ButtonSEIS.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonSEIS.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSEIS.ForeColor = System.Drawing.Color.White
         Me.ButtonSEIS.Location = New System.Drawing.Point(159, 157)
         Me.ButtonSEIS.Name = "ButtonSEIS"
         Me.ButtonSEIS.Size = New System.Drawing.Size(56, 49)
         Me.ButtonSEIS.TabIndex = 10
         Me.ButtonSEIS.Text = "6"
-        Me.ButtonSEIS.UseVisualStyleBackColor = True
+        Me.ButtonSEIS.UseVisualStyleBackColor = False
         '
         'ButtonResta
         '
-        Me.ButtonResta.Font = New System.Drawing.Font("Bauhaus 93", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonResta.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ButtonResta.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonResta.ForeColor = System.Drawing.Color.White
         Me.ButtonResta.Location = New System.Drawing.Point(236, 221)
         Me.ButtonResta.Name = "ButtonResta"
         Me.ButtonResta.Size = New System.Drawing.Size(56, 49)
         Me.ButtonResta.TabIndex = 11
         Me.ButtonResta.Text = "-"
-        Me.ButtonResta.UseVisualStyleBackColor = True
+        Me.ButtonResta.UseVisualStyleBackColor = False
         '
         'ButtonMultiplicacion
         '
-        Me.ButtonMultiplicacion.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonMultiplicacion.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ButtonMultiplicacion.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonMultiplicacion.ForeColor = System.Drawing.Color.White
         Me.ButtonMultiplicacion.Location = New System.Drawing.Point(236, 157)
         Me.ButtonMultiplicacion.Name = "ButtonMultiplicacion"
         Me.ButtonMultiplicacion.Size = New System.Drawing.Size(56, 49)
         Me.ButtonMultiplicacion.TabIndex = 12
         Me.ButtonMultiplicacion.Text = "*"
-        Me.ButtonMultiplicacion.UseVisualStyleBackColor = True
+        Me.ButtonMultiplicacion.UseVisualStyleBackColor = False
         '
         'ButtonOCHO
         '
+        Me.ButtonOCHO.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonOCHO.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonOCHO.ForeColor = System.Drawing.Color.White
         Me.ButtonOCHO.Location = New System.Drawing.Point(87, 90)
         Me.ButtonOCHO.Name = "ButtonOCHO"
         Me.ButtonOCHO.Size = New System.Drawing.Size(56, 49)
         Me.ButtonOCHO.TabIndex = 13
         Me.ButtonOCHO.Text = "8"
-        Me.ButtonOCHO.UseVisualStyleBackColor = True
+        Me.ButtonOCHO.UseVisualStyleBackColor = False
         '
         'ButtonNUEVE
         '
+        Me.ButtonNUEVE.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ButtonNUEVE.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonNUEVE.ForeColor = System.Drawing.Color.White
         Me.ButtonNUEVE.Location = New System.Drawing.Point(159, 90)
         Me.ButtonNUEVE.Name = "ButtonNUEVE"
         Me.ButtonNUEVE.Size = New System.Drawing.Size(56, 49)
         Me.ButtonNUEVE.TabIndex = 14
         Me.ButtonNUEVE.Text = "9"
-        Me.ButtonNUEVE.UseVisualStyleBackColor = True
+        Me.ButtonNUEVE.UseVisualStyleBackColor = False
         '
         'ButtonDivision
         '
-        Me.ButtonDivision.Font = New System.Drawing.Font("Bauhaus 93", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDivision.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ButtonDivision.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDivision.ForeColor = System.Drawing.Color.White
         Me.ButtonDivision.Location = New System.Drawing.Point(236, 90)
         Me.ButtonDivision.Name = "ButtonDivision"
         Me.ButtonDivision.Size = New System.Drawing.Size(56, 49)
         Me.ButtonDivision.TabIndex = 15
         Me.ButtonDivision.Text = "/"
-        Me.ButtonDivision.UseVisualStyleBackColor = True
+        Me.ButtonDivision.UseVisualStyleBackColor = False
         '
         'ButtonBorrar
         '
+        Me.ButtonBorrar.BackColor = System.Drawing.Color.Red
         Me.ButtonBorrar.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonBorrar.ForeColor = System.Drawing.Color.White
         Me.ButtonBorrar.Location = New System.Drawing.Point(125, 285)
         Me.ButtonBorrar.Name = "ButtonBorrar"
         Me.ButtonBorrar.Size = New System.Drawing.Size(43, 49)
         Me.ButtonBorrar.TabIndex = 16
         Me.ButtonBorrar.Text = "C"
-        Me.ButtonBorrar.UseVisualStyleBackColor = True
+        Me.ButtonBorrar.UseVisualStyleBackColor = False
         '
         'TextBoxResultado
         '
-        Me.TextBoxResultado.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxResultado.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.TextBoxResultado.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxResultado.ForeColor = System.Drawing.Color.White
         Me.TextBoxResultado.Location = New System.Drawing.Point(12, 24)
         Me.TextBoxResultado.Name = "TextBoxResultado"
-        Me.TextBoxResultado.Size = New System.Drawing.Size(203, 24)
+        Me.TextBoxResultado.Size = New System.Drawing.Size(245, 25)
         Me.TextBoxResultado.TabIndex = 17
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(304, 346)
         Me.Controls.Add(Me.TextBoxResultado)
         Me.Controls.Add(Me.ButtonBorrar)
